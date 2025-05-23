3b60d82bb479a1164e1df9c7265a7229cd589c1e
mohsinakabilsins <mohsinakabilsins@gmail.com>
2025-05-23 16:49:36 +0500

    'check'

--- project/backend/controllers/userController.js
+++ project/backend/controllers/userController.js
@@ -1,11 +1,13 @@
 const { User } = require('../models');
+const pool = require('../config/db');
 
 module.exports = {
   getProfile: async (req, res, next) => {
     try {
-      const user = await User.findByPk(req.user.id, {
-        attributes: ['id', 'name', 'email', 'role']
-      });
+      // const user = await User.findByPk(req.user.id, {
+      //   attributes: ['id', 'name', 'email', 'role']
+      // });
+      const user = pool.query('SELECT id, name, email, role FROM users WHERE id = $1', [req.user.id]);
       if (!user) return res.status(404).json({ message: 'User not found' });
       res.json(user);
     } catch (err) {
@@ -17,6 +19,7 @@ module.exports = {
     try {
       const { name, email } = req.body;
       const user = await User.findByPk(req.user.id);
+      const user = pool.query('SELECT * FROM users WHERE id = $1', [req.user.id]);
       if (!user) return res.status(404).json({ message: 'User not found' });
 
       user.name = name || user.name;
